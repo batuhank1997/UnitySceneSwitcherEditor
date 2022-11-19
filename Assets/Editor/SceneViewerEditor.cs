9b396f6df2c780eda6b62aa1ff11dfd2419a44e7
Batuhan <batuhank1997@hotmail.com>
2022-11-19 12:27:18 +0300

    Code refactor

--- Assets/Editor/SceneViewerEditor.cs
+++ Assets/Editor/SceneViewerEditor.cs
@@ -8,29 +8,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-[Overlay(typeof(SceneView), id: "S", displayName:"SceneViewer")]
+[Overlay(typeof(SceneView), id: ID_SCENE_VIEWER_OVERLAY, displayName:"Scene Viewer")]
 [Icon("Assets/Sprites/Icons/unity_scene.png")]
 public class SceneViewerEditor : Overlay
 {
-
     private const string ID_SCENE_VIEWER_OVERLAY = "sceneViewerOverlay";
+ 
     public override VisualElement CreatePanelContent()
     {
-        var root = new VisualElement();
+        var root = new VisualElement
+        {
+            style =
+            {
+                width = new StyleLength(new Length(120, LengthUnit.Pixel)),
+                fontSize = 10
+            }
+        };
 
-        root.style.width = new StyleLength(new Length(120, LengthUnit.Pixel));
-        
         for (int i = 0; i < EditorSceneManager.sceneCountInBuildSettings; i++)
         {
-            int index = i;
-            
+            int tempIndex = i;
+
             var sceneButton = new Button(() =>
             {
-                EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(index));
-            })
-            {
-                text = Path.GetFileName(SceneUtility.GetScenePathByBuildIndex(index))
-            };
+                if (EditorSceneManager.GetActiveScene().isDirty)
+                {
+                    EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+                }
+
+                EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(tempIndex));
+            });
+            
+            string fileName = Path.GetFileName(SceneUtility.GetScenePathByBuildIndex(tempIndex));
+
+            sceneButton.text = fileName.Substring(0, fileName.Length - 6);
 
             root.Add(sceneButton);
         }
