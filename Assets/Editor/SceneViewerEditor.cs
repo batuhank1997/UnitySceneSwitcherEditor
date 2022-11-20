c4be152450203eaefef91482fb0a82774cbd1a52
mstfmrt07 <60856457+mstfmrt07@users.noreply.github.com>
2022-11-20 03:50:24 +0300

    Show "Save Scene" dialog when scene is dirty
    Show a dialog to user when the scene is dirty, asking if they want to keep the changes or discard.

--- Assets/Editor/SceneViewerEditor.cs
+++ Assets/Editor/SceneViewerEditor.cs
@@ -29,23 +29,47 @@ public class SceneViewerEditor : Overlay
         {
             int tempIndex = i;
 
-            var sceneButton = new Button(() =>
-            {
-                if (EditorSceneManager.GetActiveScene().isDirty)
-                {
-                    EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
-                }
-
-                EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(tempIndex));
-            });
+            var sceneButton = new Button(() => ButtonCallback(tempIndex));
             
             string fileName = Path.GetFileName(SceneUtility.GetScenePathByBuildIndex(tempIndex));
 
+            //Removes the extension part of the file name (e.g: "MainScene.unity" -> "MainScene")
             sceneButton.text = fileName.Substring(0, fileName.Length - 6);
 
             root.Add(sceneButton);
         }
 
         return root;
+    }
+
+    private void ButtonCallback(int index)
+    {
+        if (EditorSceneManager.GetActiveScene().isDirty)
+        {
+            int dialogResult = EditorUtility.DisplayDialogComplex(
+                "Scene has been modified",
+                "Do you want to save the changes you made in the current scene?",
+                "Save", "Don't Save", "Cancel");
+
+            switch (dialogResult)
+            {
+                case 0: //Save and open the new scene
+                    EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+                    EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(index));
+                    break;
+                case 1: //Open the new scene without saving current.
+                    EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(index));
+                    break;
+                case 2: //Cancel process (Basically do nothing for now.)
+                    break;
+                default:
+                    Debug.LogWarning("Something went wrong when switching scenes.");
+                    break;
+            }
+        }
+        else
+        {
+            EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(index));
+        }
     }
 }
